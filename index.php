531a92dc3229eed54ff533e1b98b9b0f27dbc032
ART_mj1 <mjmjpc@naver.com>
2021-07-19 16:45:10 +0900

    07/19 ZAY 댓글 수정, 삭제 완료

--- index.php
+++ index.php
@@ -1,194 +1,186 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1" />
+  <title>Zay Shop</title>
   <!-- Favicon Link -->
   <link rel="shortcut icon" href="/zay/img/favicon.ico" type="image/x-icon">
   <link rel="icon" href="/zay/img/favicon.ico" type="image/x-icon">
   <link rel="apple-touch-icon" href="/zay/img/favicon.ico">
-  <title>ZAY shop</title>
-  <!-- font awesome link -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link
-    href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700&family=Roboto:wght@100;300;400;500;700&display=swap"
-    rel="stylesheet">
-  <!--reset css -->
-  <link rel="stylesheet" href="/zay/css/reset.css" />
-  <!--Light Slider plugin css -->
-  <link rel="stylesheet" href="/zay/lib/lightslider.css" />
-  <!-- main css -->
-  <link rel="stylesheet" href="/zay/css/style.css" />
-  <!-- media style css -->
-  <link rel="stylesheet" href="/zay/css/media.css" />
+  <!-- Font Awesome Link -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"/>
+  <!-- Reset CSS Link -->
+  <link rel="stylesheet" href="/zay/css/reset.css">
+  <!-- Light Slider Plugin Link -->
+  <link rel="stylesheet" href="/zay/lib/lightslider.css">
+  <!-- Main Style CSS Link -->
+  <link rel="stylesheet" href="/zay/css/style.css">
+  <!-- Media Style CSS Link -->
+  <link rel="stylesheet" href="/zay/css/media.css">
 </head>
-
 <body>
+
   <div class="wrap">
 
-    <?php
-    include $_SERVER["DOCUMENT_ROOT"]."/zay/include/header.php";
-    include $_SERVER["DOCUMENT_ROOT"]."/connect/db_conn.php";
-
+    <?php 
+      include $_SERVER["DOCUMENT_ROOT"]."/zay/include/header.php"; 
+      include $_SERVER["DOCUMENT_ROOT"]."/connect/db_conn.php";
     ?>
 
-    <!-- slider landing section -->
+    <!-- Slider Landing Section -->
     <section class="slider">
-      <!-- loop slider box -->
+      <!-- Loop Slider Box -->
       <div class="slider_box">
         <div class="center slider_wrap">
           <div class="slider_txt">
-            <h2>Proident occaecat</h2>
-            <h4>Ullamco laboris nisi ut</h4>
-            <p>We bring you 100% free CSS templates for your websites. If you wish to support TemplateMo, please make a
-              small contribution via PayPal or tell your friends about our website. Thank you.</p>
+            <h2>Zay eCommerce</h2>
+            <h4>Tiny and Perfect eCommerce Template</h4>
+            <p>Zay Shop is an eCommerce HTML5 CSS template with latest version of Bootstrap 5 (beta 1).<br>This template is 100% free provided by TemplateMo website.<br>Image credits go to Freepik Stories, Unsplash and Icons 8.</p>
           </div>
           <div class="slider_img">
             <img src="/zay/img/banner_img_01.jpg" alt="">
           </div>
         </div>
       </div>
-      <!-- enc of loop slider box -->
-      <!-- loop slider box -->
+      <!-- End of Loop Slider Box -->
+      <!-- Loop Slider Box -->
       <div class="slider_box">
         <div class="center slider_wrap">
           <div class="slider_txt">
-            <h2>Proident occaecat</h2>
-            <h4>Ullamco laboris nisi ut</h4>
-            <p>We bring you 100% free CSS templates for your websites. If you wish to support TemplateMo, please make a
-              small contribution via PayPal or tell your friends about our website. Thank you.</p>
+            <h2>Zay eCommerce</h2>
+            <h4>Tiny and Perfect eCommerce Template</h4>
+            <p>Zay Shop is an eCommerce HTML5 CSS template with latest version of Bootstrap 5 (beta 1).<br>This template is 100% free provided by TemplateMo website.<br>Image credits go to Freepik Stories, Unsplash and Icons 8.</p>
           </div>
           <div class="slider_img">
             <img src="/zay/img/banner_img_02.jpg" alt="">
           </div>
         </div>
       </div>
-      <!-- enc of loop slider box -->
-      <!-- loop slider box -->
+      <!-- End of Loop Slider Box -->
+      <!-- Loop Slider Box -->
       <div class="slider_box">
         <div class="center slider_wrap">
           <div class="slider_txt">
-            <h2>Proident occaecat</h2>
-            <h4>Ullamco laboris nisi ut</h4>
-            <p>We bring you 100% free CSS templates for your websites. If you wish to support TemplateMo, please make a
-              small contribution via PayPal or tell your friends about our website. Thank you.</p>
+            <h2>Zay eCommerce</h2>
+            <h4>Tiny and Perfect eCommerce Template</h4>
+            <p>Zay Shop is an eCommerce HTML5 CSS template with latest version of Bootstrap 5 (beta 1).<br>This template is 100% free provided by TemplateMo website.<br>Image credits go to Freepik Stories, Unsplash and Icons 8.</p>
           </div>
           <div class="slider_img">
             <img src="/zay/img/banner_img_03.jpg" alt="">
           </div>
         </div>
       </div>
-      <!-- enc of loop slider box -->
+      <!-- End of Loop Slider Box -->
     </section>
-    <!-- end of slider landing section -->
+    <!-- End of Slider Landing Section -->
 
     <!-- Categories Section -->
     <section class="categories">
       <div class="center">
         <div class="tit_box">
           <h2>Categories of The Month</h2>
-          <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt<br>mollit anim id est
-            laborum.</p>
+          <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt<br> mollit anim id est laborum.</p>
         </div>
         <div class="cate_box">
-          <?php
+          <?php 
+          //상품 카테고리 분류 배열
           $cate_arr = array('watches', 'shoes', 'accessories');
+          
           for($i = 0; $i < count($cate_arr); $i++){
-            $sql = "SELECT * FROM zay_pro WHERE ZAY_pro_cate='{$cate_arr[$i]}'ORDER BY ZAY_pro_idx DESC LIMIT 1";
+            $sql = "SELECT * FROM zay_pro WHERE ZAY_pro_cate='{$cate_arr[$i]}' ORDER BY ZAY_pro_idx DESC LIMIT 1";
 
             $cate_result = mysqli_query($dbConn, $sql);
             $cate_result_row = mysqli_fetch_array($cate_result);
 
             $cate_img = $cate_result_row['ZAY_pro_img_01'];
-            $cate_tit = $cate_result_row['ZAY_pro_cate']
+            $cate_tit = $cate_result_row['ZAY_pro_cate'];
           ?>
-          <!-- Loop of cate item -->
+          <!-- Loop of Cate Item -->
           <div class="cate_item">
             <div class="cate_img">
               <img src="/zay/data/product_imgs/<?=$cate_img?>" alt="">
-            </div>
+            </div>       
             <h3><?=$cate_tit?></h3>
-
             <a href="#" class="main_btn">Go Shop</a>
           </div>
-          <!-- end of Loop of cate item -->
-          <?php }?>
-
-
+          <!-- End of Loop of Cate Item -->
+          <?php } ?>
         </div>
       </div>
-
     </section>
-    <!-- featured Product section -->
+    <!-- End of Categories Section -->
+
+    <!-- Featured Product Section -->
     <section class="featured">
       <div class="center">
         <div class="tit_box">
           <h2>Featured Product</h2>
-          <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt<br>mollit anim id
-            est
-            laborum.</p>
+          <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt<br> mollit anim id est laborum.</p>
         </div>
         <div class="featured_box">
 
-        <?php
+          <?php
+          $sql1 = "SELECT * FROM zay_pro ORDER BY ZAY_pro_idx DESC";
+          $pro_result = mysqli_query($dbConn, $sql1);
 
-        $sql1 = "SELECT * FROM zay_pro ORDER BY ZAY_pro_idx DESC";
-        $pro_result = mysqli_query($dbConn,$sql1);
+          while($pro_row = mysqli_fetch_array($pro_result)){
+            $pro_row_idx = $pro_row['ZAY_pro_idx'];
+            $pro_row_img = $pro_row['ZAY_pro_img_01'];
+            $pro_row_tit = $pro_row['ZAY_pro_name'];
+            $pro_row_desc = $pro_row['ZAY_pro_desc'];
+            $pro_row_pri = $pro_row['ZAY_pro_pri'];
+          ?>
 
-        while($pro_row = mysqli_fetch_array($pro_result)){
-          $pro_row_idx = $pro_row['ZAY_pro_idx'];
-          $pro_row_img = $pro_row['ZAY_pro_img_01'];
-          $pro_row_tit = $pro_row['ZAY_pro_name'];
-          $pro_row_desc = $pro_row['ZAY_pro_desc'];
-          $pro_row_pri = $pro_row['ZAY_pro_pri'];
-        ?>
-          <!-- featured loop item -->
+          <!-- Featured Loop Item -->
           <div class="featured_item">
-            <a href="/zay/pages/details/pro_detail_form.php?pro_idx=<?=$pro_row_idx?>">
             <div class="item_frame">
-              <div class="featured_img">
-                <img src="/zay/data/product_imgs/<?=$pro_row_img?>" alt="">
-              </div>
+              <a href="/zay/pages/details/pro_detail_form.php?pro_idx=<?=$pro_row_idx?>">
+                <div class="featured_img">
+                  <img src="/zay/data/product_imgs/<?=$pro_row_img?>" alt="">
+                </div>
               </a>
               <div class="like_unlike">
                 <div class="like_icons">
-                <span>좋아요 | <b> 20 </b></span>
-                <span>별로에요 | <b>11 </b> </span>
+                  <span>좋아요 | <b>20</b></span>
+                  <span>별로에요 | <b>11</b></span>
                 </div>
-                <p><i class="fa fa-krw"></i><?=$pro_row_pri?></p>
+                <p><i class="fa fa-krw"></i> <?=$pro_row_pri?></p>
               </div>
               <div class="featured_txt">
                 <h3><?=$pro_row_tit?></h3>
-                <p class="desc"><?=$pro_row_desc?></p>
+                <p class="desc"><?=$pro_row_desc?></p>   
               </div>
+              <?php
+              $sql2 = "SELECT * FROM zay_review WHERE ZAY_pro_rev_con_idx=$pro_row_idx";
+              $rev_num_sql = mysqli_query($dbConn, $sql2);
+              $rev_total = mysqli_num_rows($rev_num_sql);
+              ?>
               <div class="reviews">
-                <em>Comments(12)</em>
+                <em>Comments(<?=$rev_total?>)</em>
               </div>
-            </div>
+            </div>     
           </div>
-          <!-- end of featured loop item -->
+          <!-- End of Featured Loop Item -->
 
           <?php } ?>
+
         </div>
         <div class="load_more">
           <button type="button">더보기</button>
         </div>
       </div>
     </section>
+    <!-- End of Featured Product Section -->
 
-    <?php
-    include $_SERVER["DOCUMENT_ROOT"]."/zay/include/footer.php";
-    ?>
+    <?php include $_SERVER["DOCUMENT_ROOT"]."/zay/include/footer.php"; ?>
   </div>
-  <!— jquery framework load —>
+
+  <!-- jQuery Framework Load -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="/zay/lib/lightslider.js"></script>
   <script src="/zay/js/jq.main.js"></script>
   <script src="/zay/js/slider.js"></script>
 </body>
-
 </html>
--- pages/details/pro_detail_form.php
+++ pages/details/pro_detail_form.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
- <meta charset="UTF-8">
- <meta http-equiv="X-UA-Compatible" content="IE=edge">
- <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, maximum-scale=1" />
- <title> Zay Shop || Detail </title>
-   <!-- Favicon Link -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1" />
+  <title>Zay Shop || Detail</title>
+  <!-- Favicon Link -->
   <link rel="shortcut icon" href="/zay/img/favicon.ico" type="image/x-icon">
   <link rel="icon" href="/zay/img/favicon.ico" type="image/x-icon">
   <link rel="apple-touch-icon" href="/zay/img/favicon.ico">
   <!-- Font Awesome Link -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <!-- Reset Css Link -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"/>
+  <!-- Reset CSS Link -->
   <link rel="stylesheet" href="/zay/css/reset.css">
   <!-- Main Style CSS Link -->
   <link rel="stylesheet" href="/zay/css/style.css">
@@ -19,144 +19,217 @@
   <link rel="stylesheet" href="/zay/css/media.css">
 </head>
 <body>
- <div class="wrap">
+  <div class="wrap">
 
-  <?php include $_SERVER["DOCUMENT_ROOT"]."/zay/include/header.php"; ?>
+    <?php include $_SERVER["DOCUMENT_ROOT"]."/zay/include/header.php"; ?>
 
-  <section class="pro_insert">
-   <div class="center">
-    <div class="detail_contents">
+    <section class="pro_insert">
+      <div class="center">
+        <div class="detail_contents">
 
-    <?php
-      $pro_idx = $_GET['pro_idx'];
-      include $_SERVER["DOCUMENT_ROOT"]."/connect/db_conn.php";
-      $sql = "SELECT * FROM zay_pro WHERE ZAY_pro_idx='{$pro_idx}'";
-      $detail_result= mysqli_query($dbConn,$sql);
-      $detail_row = mysqli_fetch_array($detail_result);
+          <?php
+          $pro_idx = $_GET['pro_idx'];
+          include $_SERVER["DOCUMENT_ROOT"]."/connect/db_conn.php";
+          $sql = "SELECT * FROM zay_pro WHERE ZAY_pro_idx='{$pro_idx}'";
+          
+          $detail_result = mysqli_query($dbConn, $sql);
+          $detail_row = mysqli_fetch_array($detail_result);
 
-      $detail_img1 = $detail_row['ZAY_pro_img_01'];
-      $detail_img2 = $detail_row['ZAY_pro_img_02'];
-      $detail_tit = $detail_row['ZAY_pro_name'];
-      $detail_pri = $detail_row['ZAY_pro_pri'];
-      $detail_desc = $detail_row['ZAY_pro_desc'];
-      $detail_color = $detail_row['ZAY_pro_color'];
-      $detail_bran = $detail_row['ZAY_pro_bran'];
-    ?>
-     <div class="detail_img">
-       <img src="/zay/data/product_imgs/<?=$detail_img1?>" alt="">
-       <img src="/zay/data/product_imgs/<?=$detail_img2?>" alt="">
-       <div class="detail_tab_btns">
-        <span><img src="/zay/data/product_imgs/<?=$detail_img1?>" alt=""></span>
-        <span><img src="/zay/data/product_imgs/<?=$detail_img2?>" alt=""></span>
-       </div>
-     </div>
-     <div class="detail_txt">
-       <div class="detail_wrap">
-         <div class="detail_top">
-           <h2><?=$detail_tit?></h2>
-           <p><span><i class="fa fa-krw"></i><?=$detail_pri?></span></p>
-           <div class="detail_like">
-            <div class="like_unlike">
-             <span>좋아요 | <b> 20 </b></span>
-             <span>별로에요 | <b>11 </b> </span>
+          $detail_img_1 = $detail_row['ZAY_pro_img_01'];
+          $detail_img_2 = $detail_row['ZAY_pro_img_02'];
+          $detail_tit = $detail_row['ZAY_pro_name'];
+          $detail_pri = $detail_row['ZAY_pro_pri'];
+          $detail_desc = $detail_row['ZAY_pro_desc'];
+          $detail_color = $detail_row['ZAY_pro_color'];
+          $detail_bran = $detail_row['ZAY_pro_bran'];
+          ?>
+
+          <div class="detail_img">
+            <img src="/zay/data/product_imgs/<?=$detail_img_1?>" alt="">
+            <img src="/zay/data/product_imgs/<?=$detail_img_2?>" alt="">
+            <div class="detail_tab_btns">
+              <span><img src="/zay/data/product_imgs/<?=$detail_img_1?>" alt=""></span>
+              <span><img src="/zay/data/product_imgs/<?=$detail_img_2?>" alt=""></span>
             </div>
-            <p class="gray">Brands : <?=$detail_bran?></p>
-            <div class="detail_desc">
-             <h3>상품설명</h3>
-             <p> 딘딘과 민경훈은 오는 19일 록발라드 곡을 발매할 예정이다. 14일 엑스포츠뉴스 취재 결과, 딘딘과 민경훈은 오는 19일 록발라드 곡을 발매할 예정이다. 딘딘과 민경훈은 지난해 우주힙쟁이(김희철, 민경훈) 작업으로 함께한 바 있다.</p>
-            </div>
-            <p class="gray">Available Color: White/ Black</p>
           </div>
-          <!-- End of detail_like -->
-         </div>
-         
+          <div class="detail_txt">
+            <div class="detail_wrap">
+              <div class="detail_top">
+                <h2><?=$detail_tit?></h2>
+                <p><span><i class="fa fa-krw"></i> <?=$detail_pri?></span></p>
+                <div class="detail_like">
+                  <div class="like_unlike">
+                    <span>좋아요 | <b>20</b></span>
+                    <span>별로에요 | <b>11</b></span>
+                  </div>     
+                  <p class="gray">Brand : <?=$detail_bran?></p>
+                  <div class="detail_desc">
+                    <h3>상품설명</h3>
+                    <p><?=$detail_desc?></p>
+                  </div>
+                  <p class="gray">Available Color : <?=$detail_color?></p>       
+                </div>
+                <!-- end of detail_like -->
+              </div>
 
-         <div class="size_quan">
-           <div class="size">
-            <p>Size:
-             <span>S</span>
-             <span>M</span>
-             <span>L</span>
-             <span>XL</span>
-            </p>
-            <p>Quality
-             <span>-</span>
-             <b>1</b>
-             <span>+</span>
-            </p>
-           </div>
-            <div class="detail_btns">
-             <button type="button">BUY NOW</button>
-             <button type="button">ADD TO CART</button>
+              <div class="size_quan">
+                <div class="size">
+                  <p>Size : 
+                    <span>S</span>
+                    <span>M</span>
+                    <span>L</span>
+                    <span>XL</span>
+                  </p>
+                  <p>Quantity 
+                    <span id="minus">-</span>
+                    <b id="count">1</b>
+                    <span id="plus">+</span>
+                  </p>
+                </div>
+                <div class="detail_btns">
+                  <button type="button">BUY NOW</button>
+                  <button type="button">ADD TO CART</button>
+                </div>
+              </div>
+              <!-- end of size quantity -->
             </div>
-         </div>
-        <!-- End of size_quantity -->
-      </div>
-      </div>
-    </div>
-    <!-- End of detail Contents -->
-   </div>
-  </section>
-  
-  <section class="comments">
-   <div class="center">
-    <div class="comments_tit">
-     <span>상품평: </span>
-     <span><em>15</em> Comments</span>
-    </div>
-    <div class="comment_insert">
-      <form action="#" method="post" name="comment_form">
-        <textarea type="text" placeholder="상품평을 입력해 주세요." 
-        name="comment_txt"> </textarea>
-        <button type="button">입력</button>
-      </form>
-    </div>
-    <div class="comment_contents">
-      <!--Loop Comments-->
-      <div class="loop_contents">
-        <div class="comments_tit">
-          <span>mjmjpc</span>
-          <span>2021-07-16 14:36:15</span>
+          </div>
         </div>
-        <div class="comments_text">
-          <span class="txt"> 
-            <em>상품이 별로에요. 배송도 느려요.</em> 
-          </span>
-          <span class="comment_btns">
-          <button type="button">수정</button>
-          <button type="button">삭제</button>
-          </span>
-        </div>
+        <!-- end of detail-contents -->
       </div>
-      <!--End of Loop Comments-->
-      <!--Loop Comments-->
-      <div class="loop_contents">
-        <div class="comments_tit">
-          <span>pink12</span>
-          <span>2021-07-16 14:36:15</span>
-        </div>
-        <div class="comments_text">
-          <span class="txt"> 
-            <em>상품이 멋있어요.</em> 
-          </span>
-          <span class="comment_btns">
-            <button type="button">수정</button>
-            <button type="button">삭제</button>
-          </span>
-        </div>
-      </div>
-      <!--End of Loop Comments-->
-    </div>
-   </div>
-  </section>
+    </section>
 
-  <?php include $_SERVER["DOCUMENT_ROOT"]."/zay/include/footer.php";?>
+    <section class="comments">
+      <div class="center">
+        <div class="comments_tit">
+          <span>상품평</span> |
+          <?php
+          include $_SERVER["DOCUMENT_ROOT"]."/connect/db_conn.php";
+          $sql_rev = "SELECT * FROM zay_review WHERE ZAY_pro_rev_con_idx=$pro_idx ORDER BY ZAY_pro_rev_idx DESC";
 
- </div>
-  <!--jQuery Framework Load-->
- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
- <script src="/zay/js/jq.main.js"></script>
- 
+          $rev_result = mysqli_query($dbConn, $sql_rev);
+          $rev_total = mysqli_num_rows($rev_result);
+
+          //echo $rev_total;
+          ?>
+          <span><em><?=$rev_total?></em> Comments</span>
+        </div>
+        <div class="comment_insert">
+          <form action="/zay/php/comment_insert.php?pro_idx=<?=$pro_idx?>&pro_writer=<?=$userid?>" method="post" name="comment_form">
+            <textarea type="text" placeholder="상품평을 입력해 주세요." name="comment_txt"></textarea>
+            <?php if(!$userid){ ?>
+            <button type="button" onclick="plzLogin()">입력</button>
+            <?php } else { ?>
+            <button type="button" onclick="insertTxt()">입력</button>
+            <?php } ?>
+          </form>
+        </div>
+        <div class="comment_contents">
+          <?php
+            
+            while($rev_row = mysqli_fetch_array($rev_result)){
+              $rev_idx = $rev_row['ZAY_pro_rev_idx'];
+              $rev_writer = $rev_row['ZAY_pro_rev_id'];
+              $rev_reg = $rev_row['ZAY_pro_rev_reg'];
+              $rev_txt = $rev_row['ZAY_pro_rev_txt'];
+              $rev_pro_idx = $rev_row['ZAY_pro_rev_con_idx'];
+          ?>
+          <!-- Loop Comments -->
+          <div class="loop_contents">
+            <div class="comments_tit">
+              <span><?=$rev_writer?></span> |
+              <span><?=$rev_reg?></span>
+            </div>    
+            <form action="/zay/php/comment_update.php?pro_idx=<?=$rev_idx?>&pro_writer=<?=$rev_writer?>" method="post">    
+              <div class="comments_text">        
+                <em class="rev_txt"><?=$rev_txt?></em>
+                <textarea class="rev_update_txt" name="rev_update_txt"><?=$rev_txt?></textarea> 
+              <?php
+               if(!$userid){ 
+              ?>
+                <input type="hidden">
+              <?php
+                } else { 
+                  if($userid != $rev_writer){  
+                ?>
+                <input type="hidden">
+              <?php 
+                } else {  
+              ?>
+                <span class="comment_btns">
+                  <button type="submit" class="rev_send">보내기</button>
+                  <button type="button" class="rev_upadte">수정</button>
+                  <button type="button" class="rev_delete" value="<?=$rev_idx?>">삭제</button>
+                  <input type="hidden" value="<?=$rev_writer?>">
+                </span>      
+              <?php
+                  }
+                }
+              ?>       
+              </div> 
+            </form>     
+          </div>
+          <!-- End of Loop Comments -->
+          <?php } ?>  
+        </div>
+      </div>
+    </section>
+
+    <?php include $_SERVER["DOCUMENT_ROOT"]."/zay/include/footer.php"; ?>
+
+  </div>
+
+  <!-- jQuery Framework Load -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="/zay/js/jq.main.js"></script>
+  <script>
+    $(function(){
+      $(".rev_upadte").click(function(){
+        $(this).toggleClass("on");
+
+        if($(this).hasClass("on")){
+          $(this).text('수정취소');
+          $(this).prev(".rev_send").show();
+          $(this).parent(".comment_btns").siblings(".rev_txt").hide();
+          $(this).parent(".comment_btns").siblings(".rev_update_txt").show();
+        } else {
+          $(this).text('수정');
+          $(this).prev(".rev_send").hide();
+          $(this).parent(".comment_btns").siblings(".rev_txt").show();
+          $(this).parent(".comment_btns").siblings(".rev_update_txt").hide();
+        }
+      });
+
+      $(".rev_delete").click(function(){
+
+        const confirmCheck = confirm('정말 삭제하시겠습니까?');
+        console.log(confirmCheck);
+
+        if(!confirmCheck){
+          return false;
+        } else {
+          const del_val = $(this).val();
+          const pro_writer = $(this).next("input").val();
+          location.href=`/zay/php/comment_delete.php?del_key=${del_val}&pro_writer=${pro_writer}`;
+        }
+
+      });
+    });
+  </script>
+  <script>
+    function plzLogin(){
+      alert('로그인 후 이용해 주세요.');
+      return false;
+    }
+
+    function insertTxt(){
+      if(!document.comment_form.comment_txt.value){
+        alert('상품평을 입력해 주세요.');
+        return;
+      }
+
+      document.comment_form.submit();
+    }
+  </script>
 </body>
 </html>
-
