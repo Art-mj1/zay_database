1b049a8e8a0fe8219af5b8d8f3787b2357f954c0
ART_mj1 <mjmjpc@naver.com>
2021-07-28 14:49:21 +0900

    07/28 분류작업 완료 카테고리별 분류 및 좋아요순, 가격순,새상품순 포함

--- pages/menu_page/shop.php
+++ pages/menu_page/shop.php
@@ -30,17 +30,34 @@
 
      <div class="shop_btns">
        <div class="tabs">
-        <button type="button">All</button>
-        <button type="button">Watches</button>
-        <button type="button">Shoes</button>
-        <button type="button">Accessories</button>
+        <a href="?key=all" class="active">전체보기</a>
+        <a href="?key=watches">시계</a>
+        <a href="?key=shoes">신발</a>
+        <a href="?key=accessories">액세서리</a>
        </div>
        <div class="filters">
-        <button type="button">새상품순</button>
-        <button type="button">좋아요순</button>
-        <button type="button">가격순</button>
+         <div class="filter_tabs">
+           <select onchange="location.href=this.value">
+             <option selected disable value="">검색조건</option>
+             <option value="?key=new">새상품순</option>
+             <option value="?key=like">좋아요순</option>
+             <option value="?key=price">가격순</option>
+           </select>
+         </div>
+          <div class="search">
+           <input type="text" style="border:1px solid;">
+           <i class="fa fa-search"></i>
+          </div>
        </div>
+     </div> <!--End of shop btns-->
+     
+     <div class="featured_box">
+      <?php include $_SERVER['DOCUMENT_ROOT']."/zay/data/sort/category_sort.php"?>
      </div>
+     <div class="load_more">
+        <button type="button">더보기</button>
+     </div>
+    </div>
    </div>
  </section>
 
@@ -48,7 +65,28 @@
  <?php include $_SERVER["DOCUMENT_ROOT"]."/zay/include/footer.php";?>
 
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+ <script src="/zay/js/jq.like.unlike.js"></script>
  <script src="/zay/js/jq.main.js"></script>
+ <script>
+   const pathName = window.location.href;
+  //  console.log(pathName);
+
+  const btns= document.querySelectorAll('.shop .shop_btns a');
+  // console.log(btns);
+  const btnsArr = ['all','watches','shoes','accessories'];
+
+  for(let i=0; i<btnsArr.length;i++){
+    btns[i].classList.remove('active');
+    if(pathName.includes(btnsArr[i])){
+      btns[i].classList.add('active');
+    }
+  }
+
+  function plzLogin(){
+      alert('로그인 후 이용해 주세요.');
+      return false; //로그인 창으로 뜨게 설정해도됨 location.href();
+    };
+ </script>
 </body>
 </html>
 
